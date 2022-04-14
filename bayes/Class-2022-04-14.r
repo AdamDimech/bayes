a6ffc8a7d3b04cc873c286212f9204f322738b82
Adam Dimech <a.dimech@adonline.id.au>
2022-04-14 13:58:19 +1000

    Update file header

--- bayes/Class-2022-04-14.r
+++ bayes/Class-2022-04-14.r
@@ -1,16 +1,18 @@
-# From Josquin on 2022-04-14
+# PFG class hosted by Josquin on 2022-04-14
 
 G <- 1000
 
 N = 2000
 observations <- rbinom(N, 1, 0.3)
-water-as.numeric(sum(observations))
+water=as.numeric(sum(observations))
 Land=N-water
 
 # Define grid
 pGrid <- seq( from=0, to=1, length.out=G)
 
-prior <- rep(1, G)
+#prior <- rep(1, G)
+prior <- ifelse (pGrid < 0.5, 0.01, 0.09)
+
 
 # W ~ Binomial (N, p)
 likelihood <- dbinom(water, size = water+Land, prob = pGrid)
@@ -28,10 +30,10 @@ unstdPosterior <- likelihood * prior
 # Standardise posterior
 posterior <- unstdPosterior / sum(unstdPosterior)
 
+
 plot( pGrid, posterior, type="b", xlab="Probability of water", ylab="Posterior probability")
-mtext (paste(G, "points"))
+
+plot( pGrid, prior, type="b", xlab="Probability of water", ylab="Posterior probability")
 
 # What is the most likely value of p(water)
 which(posterior %in% c(max(posterior)))/G
-
-
