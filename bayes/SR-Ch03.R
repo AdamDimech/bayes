76ab9be304632a3d3546f9081a7d17529d6b4e7d
Adam Dimech <a.dimech@adonline.id.au>
2022-05-04 11:36:43 +1000

    Probability of vampirism (Rcode 3.1)

--- bayes/SR-Ch03.R
+++ bayes/SR-Ch03.R
@@ -1,1 +1,11 @@
 # Statistical Rethinking: Chapter 3
+
+# Rcode 3.1
+Pr_Positive_Vampire <- 0.95
+Pr_Positive_Mortal <- 0.01
+Pr_Vampire <- 0.001
+Pr_Positive <- Pr_Positive_Vampire * Pr_Vampire + Pr_Positive_Mortal * (1-Pr_Vampire)
+(Pr_Vampire_Positive <- Pr_Positive_Vampire*Pr_Vampire / Pr_Positive)
+
+# Output: [1] 0.08683729
+# = 87% chance
