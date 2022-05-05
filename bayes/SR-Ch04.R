af0983bad87e70a96a585054c572a33c292536e4
Adam Dimech <a.dimech@adonline.id.au>
2022-05-05 15:49:43 +1000

    Normal by addition (Rcode 4.1)

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -1,1 +1,14 @@
 # Statistical Rethinking: Chapter 4
+
+#Rcode 4.1
+pos <-replicate(1000, sum(runif(16, -1, 1)))
+hist(pos)
+plot(density(pos))
+
+png(filename="plots/SR-4.1-histogram-of-positions.png")
+hist(pos)
+dev.off()
+
+png(filename="plots/SR-4.1-density-positions.png")
+plot(density(pos))
+dev.off()
