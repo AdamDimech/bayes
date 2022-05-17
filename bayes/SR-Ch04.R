eed77650df29d2f64e5368e7ed4698aeccd47cca
Adam Dimech <a.dimech@adonline.id.au>
2022-05-17 14:19:04 +1000

    Normal by multiplication (Rcode 4.2/4.3)

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -1,5 +1,7 @@
 # Statistical Rethinking: Chapter 4
 
+library(rethinking)
+
 #Rcode 4.1
 pos <-replicate(1000, sum(runif(16, -1, 1)))
 hist(pos)
@@ -12,3 +14,10 @@ dev.off()
 png(filename="plots/SR-4.1-density-positions.png")
 plot(density(pos))
 dev.off()
+
+#Rcode 4.2
+prod(1+runif(12,0,0.1))
+
+#Rcode 4.3
+growth <- replicate(1000, prod(1+runif(12,0,0.1)))
+dens(growth, norm.comp=TRUE)
