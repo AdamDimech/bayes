4beb999b1316b057f7a9b49fea6113ed17761d71
Adam Dimech <a.dimech@adonline.id.au>
2022-06-08 11:39:24 +1000

    Chapter 4 (Rplots 4.12 to 4.18)

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -61,3 +61,51 @@ str(d)
 precis(d, hist=FALSE)
 d$height
 d2 <- d[d$age >=18, ]
+
+# p.80
+png(filename="plots/SR-4.11-density-plot-heights-adults.png")
+dens(d2$height)
+dev.off()
+
+# Rcode 4.12
+png(filename="plots/SR-4.12-mean-prior.png")
+curve(dnorm(x, 178, 20), from=100, to=250)
+dev.off()
+
+# Rcode 4.13
+png(filename="plots/SR-4.13-standard-deviation-prior.png")
+curve(dunif(x, 0, 50), from=-10, to=60)
+dev.off()
+
+# Rcode 4.14 Sampling from the prior
+sample_mu <- rnorm(1e4, 178, 20)
+sample_sigma <- runif(1e4, 0, 50)
+prior_h <- rnorm(1e4, sample_mu, sample_sigma)
+png(filename="plots/SR-4.14-sampling-from-the-prior.png")
+dens(prior_h)
+dev.off()
+
+# Rcode 4.15
+sample_mu <- rnorm(1e4, 178, 100)
+prior_h <- rnorm(1e4, sample_mu, sample_sigma)
+png(filename="plots/SR-4.15-sampling-from-the-prior-less-informative.png")
+dens(prior_h)
+dev.off()
+
+# Rcode 4.16
+mu.list <- seq(from=150, to=160, length.out=100)
+sigma.list <- seq(from=7, to=9, length.out=100)
+post <- expand.grid(mu=mu.list, sigma=sigma.list)
+post$LL <- sapply (1:nrow(post), function(i) sum(dnorm(d2$height, post$mu[i], post$sigma[i], log=TRUE)))
+post$prod <- post$LL + dnorm(post$mu, 178, 20, TRUE) + dunif(post$sigma, 0, 50, TRUE)
+post$prob <- exp(post$prod - max(post$prod))
+
+# Rcode 4.17
+png(filename="plots/SR-4.17-contour-plot.png")
+contour_xyz (post$mu, post$sigma, post$prob)
+dev.off()
+
+# Rcode 4.18
+png(filename="plots/SR-4.18-heat-plot.png")
+image_xyz (post$mu, post$sigma, post$prob)
+dev.off()
