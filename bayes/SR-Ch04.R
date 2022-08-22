cf011ec5e57fe7196b4e971f55078b798e8aca96
Adam Dimech <a.dimech@adonline.id.au>
2022-08-22 14:16:22 +1000

    Rcode 4.40

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -256,3 +256,19 @@ png(filename="plots/SR-4.40-log-normal-beta.png")
 dens(b, xlim(c(0, 5)), adj=0.1)
 dev.off()
 
+# Rcode 4.41
+
+set.seed(2971)
+N <- 100
+a <- rnorm(N, 178, 20)
+b <- rnorm(N, 0, 1)
+
+plot( NULL , xlim=range(d2$weight) , ylim=c(-100,400) ,
+      xlab="weight" , ylab="height" )
+abline( h=0 , lty=2 )
+abline( h=272 , lty=1 , lwd=0.5 )
+mtext( "b ~ dnorm(0,10)" )
+xbar <- mean(d2$weight)
+for ( i in 1:N ) curve( a[i] + b[i]*(x - xbar) ,
+                        from=min(d2$weight) , to=max(d2$weight) , add=TRUE ,
+                        col=col.alpha("black",0.2) )
