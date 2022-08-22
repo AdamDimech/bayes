6a45482d8607ac7a91084023d430bf3a5ac5f00e
Adam Dimech <a.dimech@adonline.id.au>
2022-08-22 14:08:04 +1000

    Plot lines from 100 pairs of α and β values (Rcode 4.39)

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -233,3 +233,17 @@ set.seed(2971)
 N <- 100
 a <- rnorm(N, 178, 20)
 b <- rnorm(N, 0, 10)
+
+# Rcode 4.39
+
+png(filename="plots/SR-4.39-height-weight-alpha-beta.png")
+plot( NULL , xlim=range(d2$weight) , ylim=c(-100,400) ,
+      xlab="weight" , ylab="height" )
+abline( h=0 , lty=2 )
+abline( h=272 , lty=1 , lwd=0.5 )
+mtext( "b ~ dnorm(0,10)" )
+xbar <- mean(d2$weight)
+for ( i in 1:N ) curve( a[i] + b[i]*(x - xbar) ,
+                        from=min(d2$weight) , to=max(d2$weight) , add=TRUE ,
+                        col=col.alpha("black",0.2) )
+dev.off()
