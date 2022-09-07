f11a614c44c0c020eaa850ff2a926a3d43d7c271
Adam Dimech <a.dimech@adonline.id.au>
2022-09-07 14:24:02 +1000

    Rcode 4.69 (Error)

--- bayes/SR-Ch04.R
+++ bayes/SR-Ch04.R
@@ -465,3 +465,31 @@ shade( mu.PI , weight.seq )
 shade( height.PI , weight.seq )
 dev.off()
 
+# Rcode 4.69 # Does not work
+d$weight_s3 <- d$weight_s^3
+m4.6 <- quap(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + b1*weight_s + b2*weight_s2 + b3*weight_s3 ,
+    a ~ dnorm( 178 , 20 ) ,
+    b1 ~ dlnorm( 0 , 1 ) ,
+    b2 ~ dnorm( 0 , 10 ) ,
+    b3 ~ dnorm( 0 , 10 ) ,
+    sigma ~ dunif( 0 , 50 )
+  ) ,
+  data=d )
+
+weight.seq <- seq( from=-2.2 , to=2 , length.out=30 )
+pred_dat <- list( weight_s=weight.seq , weight_s2=weight.seq^2, d$weight_s3 <- d$weight_s^3 )
+mu <- link( m4.6 , data=pred_dat ) # Error in eval(parse(text = lm), envir = e) : object 'weight_s3' not found
+mu.mean <- apply( mu , 2 , mean )
+mu.PI <- apply( mu , 2 , PI , prob=0.89 )
+sim.height <- sim( m4.6 , data=pred_dat )
+height.PI <- apply( sim.height , 2 , PI , prob=0.89 )
+
+plot( height ~ weight_s , d , col=col.alpha(rangi2,0.5) )
+lines( weight.seq , mu.mean )
+shade( mu.PI , weight.seq )
+shade( height.PI , weight.seq )
+
+
