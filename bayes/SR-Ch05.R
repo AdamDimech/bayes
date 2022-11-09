89206df163bd1b251963a35b0643335370e6bfc1
Adam Dimech <a.dimech@adonline.id.au>
2022-11-09 12:02:58 +1100

    Rcode 5.1

--- bayes/SR-Ch05.R
+++ bayes/SR-Ch05.R
@@ -1,3 +1,12 @@
 # Statistical Rethinking: Chapter 4
 
 library(rethinking)
+
+# Rcode 5.1
+
+data(WaffleDivorce)
+d <- WaffleDivorce
+
+d$D <- standardize(d$Divorce)
+d$M <- standardize(d$Marriage)
+d$A <- standardize(d$MedianAgeMarriage)
