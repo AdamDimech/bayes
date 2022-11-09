f1381feedc619743e224b27b49778c0c2f07061b
Adam Dimech <a.dimech@adonline.id.au>
2022-11-09 12:10:27 +1100

    Compute the approximate prior (Rcode 5.3)

--- bayes/SR-Ch05.R
+++ bayes/SR-Ch05.R
@@ -10,3 +10,18 @@ d <- WaffleDivorce
 d$D <- standardize(d$Divorce)
 d$M <- standardize(d$Marriage)
 d$A <- standardize(d$MedianAgeMarriage)
+
+# Rcode 5.2
+sd(d$MedianAgeMarriage)
+
+# Compute the approximate prior (Rcode 5.3)
+m5.1 <- quap(
+  alist(
+    D ~ dnorm(mu, sigma),
+    mu <- a + bA * A,
+    a ~ dnorm(0,0.2),
+    bA ~ dnorm(0,0.5),
+    sigma ~ dexp(1)
+  ), data=d
+)
+
