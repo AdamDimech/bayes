535ece30e8b29b1be7c5e4ebb1a4a946b58dcc3c
Adam Dimech <a.dimech@adonline.id.au>
2022-11-09 12:16:43 +1100

    Simulate from the priors (Rcode 5.4)

--- bayes/SR-Ch05.R
+++ bayes/SR-Ch05.R
@@ -25,3 +25,11 @@ m5.1 <- quap(
   ), data=d
 )
 
+# Simulate from the priors (Rcode 5.4)
+set.seed(10)
+prior <- extract.prior(m5.1)
+mu <- link(m5.1, post=prior, data=list(A=c(-2,2)))
+png(filename="plots/SR-5.4-simulate-from-priors.png")
+plot(NULL, xlim=c(-2,2), ylim=c(-2,2))
+for(i in 1:50) lines(c(-2, 2), mu[i,], col=col.alpha("black", 0.4))
+dev.off()
